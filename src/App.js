111cf7b79fff78fa20c41cfea3c7572c53810514
manvirxkaur <manvirkaur745@gmail.com>
2021-01-25 15:51:20 +0530

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
                 <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
               </div>
               </Navbar>
+              <Menu/>
             
     </div>
   );
